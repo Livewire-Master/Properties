ba8b60fc4413095829ee2aae31c6c0bbf86e331b
RealMrHex <RealMrHex@gmail.com>
2024-04-01 01:53:45 +0330

    Initializing properties

--- app/Livewire/ArticlesPage.php
+++ app/Livewire/ArticlesPage.php
@@ -2,12 +2,38 @@
 
 namespace App\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title('.:: Properties - Articles ::.')]
 class ArticlesPage extends Component
 {
-    public $isTimerOn = true;
-    public $description = 'you are viewing your articles.';
+    public $isTimerOn;
+    public $description;
+
+    public function mount()
+    {
+        $this->isTimerOn = $this->timerStatus();
+        $this->description = $this->currentDescription();
+
+        $this->fill(
+            [
+                'isTimerOn' => true,
+                'description' => 'Bulk filling....'
+            ]
+        );
+
+        // $this->fill(auth()->user()->only(['x', 'y'])); # sample
+    }
+
+    protected function timerStatus()
+    {
+        return false;
+    }
+
+    protected function currentDescription()
+    {
+        return 'You are visiting your articles at ' . Carbon::now()->format('d F Y - H:i');
+    }
 }
